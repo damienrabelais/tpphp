aff9f8daea40ea5f1388ca63f0ad4c575fa449bd
damienrabelais <damien.boucard@ac-rennes.fr>
2023-10-20 16:02:50 +0200

    Premier formulaire et traitement

--- premier.php
+++ premier.php
@@ -3,5 +3,15 @@
 <?php
 echo "Mon premier script PHP!";
 ?>
+
+
+<?php
+
+for ($x = 0; $x <= 10; $x++) {
+    echo "The number is: $x <br>";
+}
+?>
+
+
 </body>
 </html>
---
+++ welcome.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+Welcome <?php echo $_GET["txtName"]; ?><br>
+Your email address is: <?php echo $_GET["txtEmail"];
+?>
+
+</body>
+</html>
